d9bfedc4caf37e028f180f145493d39dcd74b08d
Dana-lil <dana.alajbur@gmail.com>
2025-05-23 00:46:31 +0300

    error

--- app.js
+++ app.js
@@ -52,7 +52,8 @@ const items =[
       // now formt the time before displaying it 
      let secondsValue =seconds < 10 ? `0${seconds}` : seconds;
      let minutesValue =minutes < 10 ? `0${minutes}` : minutes;
-        timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+        timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}
+        `;
         
     };
 
@@ -83,7 +84,7 @@ const items =[
             TempArray.splice(randomIndex, 1);
 
         } 
-        return cardValues
+        return cardValues;
     };
 
     const matrixGenerator = (cardValues, size =4) => {
@@ -105,7 +106,90 @@ const items =[
 
         // grid 
         gameContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+
+
+        // cards 
+
+cards= document.querySelectorAll(".card-container");
+cards.forEach((card) =>{
+    card.addEventListener("click", () =>{
+
+        if(!card.classList.contains("matched")) {
+            // flip the clicked card
+            card.classList.add("flipped");
+            if(!firstCard){
+                firstCard=card;
+                firstCardValue=card.getAttribute
+             ("data-card-value");
+            } 
+             else{
+            movesCounter();
+            secondCard =card;
+            let secondCardValue =card.getAttribute("data-card-value");
+            if(firstCardValue == secondCardValue){
+                firstCard.classList.add("matched");
+                secondCard.classList.add("matched");
+                 
+                firstCard = false;
+                winCount += 1;
+
+                if(winCount == Math.floor(cardValues.length / 2)) {
+                    result.innerHTML = `<h2> You won </h2>
+                    <h4> Moves: ${movesCount} </h4>`;
+                    stopGame();
+                    //35 seconeds to finish 
+                }
+            }
+             else {
+                // if the cards do not match then flip the cards back to normal 
+                let [tempFirst , tempSecond] = [firstCard,secondCard]; 
+                firstCard =false;
+                secondCard = false ;
+                let dalay = setTimeout(() => {
+                    tempFirst.classList.remove("flipped");
+                    tempSecond.classList.remove("flipped");
+
+                },900);
+            }
+        }
+        }
+       
+    })
+})
     };
+
+//start game 
+startButton.addEventListener("click" , () =>{
+    movesCount = 0;
+    time =0;
+
+    // working in controls and buttons visibality 
+     controls.classList.add("hide");
+    stopButton.classList.remove("hide");
+    startButton.classList.add("hide");
+
+    // start the timer when the game begins 
+    interval = setInterval(timeGenerator , 1000);
+
+    // the initial moves 
+    moves.innerHTML = `<span>Moves:</span> ${movesCount}
+    `;
+    
+initializer();
+
+});
+
+ // stop the game 
+ stopButton.addEventListener(
+    "click" , ( stopGame = () => {
+
+    controls.classList.remove("hide");
+    stopButton.classList.add("hide");
+    startButton.classList.remove("hide");
+    clearInterval(interval);
+
+ })
+);
 
 const initializer = () => {
     result.innerHTML = "";
@@ -117,4 +201,3 @@ const initializer = () => {
 
 
 
-initializer();
